88899994ce4e4f39f9e4dc4c7c0264ff50bfb727
dajvido <dawid.jaskot@gmail.com>
2015-06-29 12:24:01 +0200

    Add Roomm seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+rooms = [
+  {},
+  { occupancy: 1, bathroom: true, price: 70 },
+  { occupancy: 1, bathroom: true, standard: 'medium', price: 110 },
+  { occupancy: 2, price: 90.0 },
+  { occupancy: 2, bathroom: true, price: 110 },
+  { occupancy: 2, bathroom: true, standard: 'medium', price: 130 },
+  { occupancy: 2, bathroom: true, standard: 'high', price: 170 },
+  { occupancy: 3, price: 130.0 },
+  { occupancy: 3, bathroom: true, price: 170 },
+  { occupancy: 3, bathroom: true, standard: 'medium', price: 200 },
+  { occupancy: 3, bathroom: true, standard: 'high', price: 250 },
+]
+
+rooms.each do |room|
+  Room.create(room)
+end
